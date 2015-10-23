d70d397016728aa6a9815a97691c98ab4809fc5d
root <root@ubilinux.(none)>
2015-10-23 07:45:26 +0000

    bugfix

--- server/geofenceServer.js
+++ server/geofenceServer.js
@@ -1,7 +1,8 @@
-var GeofenceServer = function(listenPort) {
+function GeofenceServer(listenPort) {
   this.listenPort = 5555;
   this.server = null;
   this.activeArea = [];
+  this.template = null;
   this._init(listenPort);
 }
 
@@ -10,10 +11,14 @@ GeofenceServer.prototype._init = function(listenPort){
   if(typeof listenPort !== 'undefined' && listenPort) {
     _self.listenPort = listenPort;
   }
+  _self.template = {
+    result: 0,
+    data: null
+  };
 }
 
 GeofenceServer.prototype.start = function(){
-  var _self = this();
+  var _self = this;
   var restify = require('restify');
 
   _self.server = restify.createServer({
@@ -24,37 +29,31 @@ GeofenceServer.prototype.start = function(){
   _self.server.use(restify.bodyParser());
 
   // server route
-  _self.server.get('/geofence/getActiveArea', _self.getActiveArea);
-  _self.server.post('/geofence/updateActiveArea', _self.updateActiveArea);
+  _self.server.get('/geofence/getActiveArea', _self.getActiveArea.bind(_self));
+  _self.server.post('/geofence/updateActiveArea', _self.updateActiveArea.bind(_self));
 
   _self.server.listen(_self.listenPort, function(){
-    console.log('%s listening at %s', server.name, server.url)
-  })
+    console.log('%s listening at %s', _self.server.name, _self.server.url)
+  });
 }
 
 GeofenceServer.prototype.getActiveArea = function(req, res, next) {
+console.log("get active area");
   var _self = this;
-  var response = _self._getResponseTemplate();
+  var response = _self.template;
   response.data = _self.activeArea;
   res.send(response);
 }
 
 GeofenceServer.prototype.updateActiveArea = function(req, res, next) {
+console.log("update active area");
+  var _self = this;
   var params = req.params;
-  var response = _self._getResponseTemplate();
+  var response = _self.template;
 
   console.log(params);
 
   res.send(response);
-}
-
-Geofence.prototype._getResponseTemplate = function(){
-  var template = {
-    result: 0,
-    data: null
-  }
-
-  return template;
 }
 
 module.exports = GeofenceServer;
